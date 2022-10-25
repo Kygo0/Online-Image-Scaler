4deb6db45714cc7e427986916b2daf0e31474a9a
Ioanni Tsaka <iwanhstsakas2002@yahoo.com>
2022-10-26 01:26:13 +0300

    Updated website/Added CSS

--- javascript.js
+++ javascript.js
@@ -4,3 +4,17 @@ imgInp.onchange = evt => {
 		imageID.src = URL.createObjectURL(file)
 	}
 }
+
+function changeStyle() {
+    var element = document.getElementById("imageID");
+    var widthElement = document.getElementById("userWidth").value;
+    var heightElement = document.getElementById("userHeight").value;
+    if (widthElement > 650) {
+        widthElement = 650;
+    }
+    if (heightElement > 650) {
+        heightElement = 650;
+    }
+    element.style.width = widthElement + "px";
+    element.style.height = heightElement + "px";
+}
